8da72c1ab3ddf3a9c2a24e97ade762d86334416d
Patrick E <patrickkoda@gmail.com>
2021-04-10 14:54:14 -0700

    Adding Python Coding Notes

---
+++ Python_Examples.py
@@ -0,0 +1,274 @@
+#Variables
+
+#Code in this section are examples of different ways to list variables and values.
+
+
+#message = ("What it do, gir")
+
+#print (message)
+
+#message =("We back in here, hunny")
+
+#print (message)
+
+#man ="62"
+
+#woman = "40"
+
+#print(man+woman)
+
+#cup ="water"
+
+#print(cup)
+
+#cup ="Sand"
+
+#print(cup)
+
+#crate = ["Sand", "Water", 7,5.9,True] #list-variable that can hold multiple values at the same time that can be different data types. Also called an "array" in other languages. 
+#Index-Value listed in the list/array. 
+
+#print(crate[3])
+
+#Calculations
+
+#firstValue = 50
+
+#secondValue= 200
+
+#operation - addition
+
+#firstValue + secondValue 
+
+#result = firstValue operator secondValue
+
+#print(result)
+
+#operator = '+''
+
+#fstrings & methods (Code below has examples of fstrings and a method"title")
+
+
+#first_name = "ada"
+
+#last_name = "lovelace"
+
+#full_name = f"{first_name} {last_name}"
+
+#message = (f"Hello, {full_name.title()}!")
+
+#print(message)
+
+#Whitespace (Tabs, New lines, etc)
+
+#Tabs- See example below for adding tabs in code. 
+
+#print("Python")
+
+#print("\tPython")
+
+#Newlines- See example below for adding new line. 
+
+#print("Languages:\nPython\nC\nJavascript")
+
+#Concatenation- combining 2 or more strings of data together. 
+
+
+#string1 ="Mike"
+#string2 = "4.5"
+#string3 ="0"
+#string4 ="true"
+#string5="6"
+
+#print(string1 + ' does not know that to be ' +string4)
+
+#Numbers With numbers, combining is just addition, not concatenation. Cannot print a string and numbers together. 
+
+#number1 =4.5
+
+#number2 =0
+
+#number3 =5
+
+#number4 =-4
+
+#number5 =543545654
+
+#print(number1 + number3)
+
+#print(number1 + string1)
+
+#Boolean-
+
+#age =35
+
+
+
+#Is_Student_Active = True #yes
+
+#print(Is_Student_Active)
+
+#Is_Graduate = False #no
+
+#print (Is_Graduate)
+
+#if(age >45):
+   ## print('yes')
+
+#if(age<45):
+   # print("no")
+
+# name = "Mike" #string- s
+
+# age = 55 #number- d
+
+# print("His name is %s and he is %d" % (name,age))
+
+# sample_list =["Mike","Love","Dove"]
+
+# print("some text %s" % sample_list)
+
+#len()-used to know the length of a string. Length includes amount of characters and spaces between characters. 
+
+#string methods
+
+#string ="I am a string"
+
+#string2 ="mike b"
+
+#string3 ="BOOM BAYAH"
+
+#print(len(string))
+
+#print(string2.index("e"))
+
+#print(string[1:3:8])
+
+#print(string.upper())
+
+#print(string3.lower())
+
+#Operators- allows you to perform actions on one or more things. (+, -, *, /)
+
+#first_value = 4
+#second_value = 7
+
+#addition
+
+#print(first_value + second_value)
+
+#print(first_value - second_value)
+
+#print(first_value / second_value)
+
+#assignment operator = equal to sign. Used to assign a value to something. 
+
+#comparison operator(==)
+
+#print(first_value == second_value) #comparing if these two values are equal. Returns back false with current values. 
+
+#Greaterthan
+
+#print(first_value > second_value)
+
+#lessthan
+
+#print(first_value < second_value)
+
+#GreaterthanorEqualTo
+
+#print(first_value >= second_value)
+
+#reminder- Divides value and gives you the remainder from the division. 
+
+#print(first_value % second_value)
+
+#voice ="Scream "
+
+#print(voice * 5)
+
+#squad = [1,2,3,4,5,6]
+
+#print(squad *3)
+
+
+#A = [1,2,3,4]
+
+#B = [5,6,7,8]
+
+#print(A + B)
+
+
+#Conditional-expression that will only be executed if a certain condition is met. 
+#When using conditionals, must add ":" at end of condition before printing due to syntax rules.
+
+#age = 17.5
+
+#movie = "scary-terry"
+
+#if(age == 18):
+#print('Customer can purchase ticket for movie' + movie)
+#if(age < 18):
+
+   #print('Customer cannnot purchase ticket for movie')
+
+#and/or
+#and- two or more conditions must be true for test to pass.
+#or- only one condition is required to be true for test to pass. 
+
+#age = 18
+#height = 5
+
+#if(age >=18 and height >=5): #and code would pass test here.
+  # print("Allowed to ride")
+
+#if(age < 18 and height < 5) the test for and would not pass since one condition is not true. Better to use "or".
+
+#if(age < 18 or height < 5):
+   #print("Not allowed to ride")
+
+#elif- The elif keyword is pythons way of saying "if the previous conditions were not true, then try this condition".
+
+#elif(age < 18 or height < 5):
+  # print("Not allowed to ride")
+
+#else:
+   #print("Error, something went wrong, girl")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
